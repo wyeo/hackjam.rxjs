a49f07b1eb7d677f118ba8fc4f17cab970187688
davyengone <engonenzedavy@yahoo.com>
2017-01-10 18:39:44 +0100

    refacto not done

--- src/intro.js
+++ src/intro.js
@@ -1,18 +1,10 @@
+// Observable
 // an observable is a function that accepts a producer in parameter and has a subscribe method
-// a producer is  a function that throws/produces values and accepts an observer
+// Producer
+// A producer is  a function that throws/produces values and accepts an observer
+// Observer
 // an observer is just an object that has 3 functions: next, error, complete
-//  and listen to the value emitted  by the producer
-export class Observable {
-}
-/**
- * Create a producer that emit 3 times the value 10
- * and complete
- * @param observer
- */
+// and listen to the value emitted  by the producer
 
+function Observable() {}
 
-/**
- * Create a new observable that accept this tenProducer
- * and subscribe to it
- * @type {Observable}
- */
--- src/intro.spec.js
+++ src/intro.spec.js
@@ -1,54 +1,59 @@
 import {expect} from "chai";
 import sinon from "sinon";
-import {Observable} from "./intro";
 
+// Import our modules
+import { Observable } from "./intro";
+
+// Observable
 // an observable is a function that accepts a producer in parameter and has a subscribe method
-// a producer is  a function that throws/produces values and accepts an observer
+// Producer
+// A producer is  a function that throws/produces values and accepts an observer
+// Observer
 // an observer is just an object that has 3 functions: next, error, complete
 // and listen to the value emitted  by the producer
 
 describe('What is an Observable', () => {
+
   it('An Observable is a function', () => {
     expect(typeof Observable).equal('function');
   });
+
   it('An observer is a object that has 3 functions: next, error, complete', () => {
     const observer = {
-      next(val){
-      },
-      error(err){
-      },
-      complete(){
-      }
+      next(val){},
+      error(err){},
+      complete(){},
     };
+
     expect(typeof observer.next).equal('function');
     expect(typeof observer.error).equal('function');
     expect(typeof observer.complete).equal('function');
   });
+
   it('A Producer is a function that takes an observer as parameter and throws/produces values', () => {
-    const nextSpy = sinon.spy();
-    const errorSpy = sinon.spy();
-    const completeSpy = sinon.spy();
+    const next = sinon.spy();
+    const error = sinon.spy();
+    const complete = sinon.spy();
     const observer = {
-      next: nextSpy,
-      error: errorSpy,
-      complete: completeSpy
+      next,
+      error,
+      complete,
     };
     const nextValue = 10;
     const errorValue = 'an error';
-    const producer = function (observer) {
+    const producer = (observer) => {
       observer.next(10);
       observer.error(errorValue);
       observer.complete();
     };
     producer(observer);
-    expect(nextSpy.getCall(0).args[0]).equals(nextValue);
-    expect(errorSpy.getCall(0).args[0]).equals(errorValue);
-    expect(completeSpy.calledOnce);
+    expect(next.getCall(0).args[0]).equals(nextValue);
+    expect(error.getCall(0).args[0]).equals(errorValue);
+    expect(complete.calledOnce);
   });
 
   it('An Observable is a function that takes producer as parameter', () => {
-    const producer = function () {
-    };
+    const producer = () =>{};
     const spy = sinon.spy(Observable.prototype, 'constructor');
     const constructor = Observable.prototype.constructor;
     const inst = new constructor(producer);
@@ -56,21 +61,18 @@ describe('What is an Observable', () => {
   });
 
   it('An Observable is a function that has a subscribe method', () => {
-    const producer = function () {
-    };
+    const producer = () =>{};
     const observable = new Observable(producer);
     expect(typeof observable.subscribe).equal('function');
   });
 
   it('The subscribe method of an Observable takes a observer as parameter', () => {
-    const producer = function () {
-    };
+    const producer = () =>{};
     const observable = new Observable(producer);
     const observer = {
-      next(val){
-      }, error(err){
-      }, complete(){
-      }
+      next(val){},
+      error(err){},
+      complete(){},
     };
 
     const spy = sinon.spy(observable, 'subscribe');
@@ -84,13 +86,13 @@ describe('What is an Observable', () => {
   it('The subscribe method of an Observable call the producer with the observer as parameter', () => {
     const nextValue = 10;
     const errorValue = 'an error';
-    const nextSpy = sinon.spy();
-    const errorSpy = sinon.spy();
-    const completeSpy = sinon.spy();
+    const next = sinon.spy();
+    const error = sinon.spy();
+    const complete = sinon.spy();
     const observer = {
-      next: nextSpy,
-      error: errorSpy,
-      complete: completeSpy
+      next,
+      error,
+      complete,
     };
     const producer = function (observer) {
       observer.next(10);
